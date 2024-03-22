a43ab31bda79a055a1aab5de9e91d2c2d032d8e4
Hoàng Kim <100124784+NguyenBaHoangKim@users.noreply.github.com>
2024-03-23 01:20:20 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string xetHB(int DRL, bool diemF, int tongTC, float tbhk, bool kyLuat){
+    if (DRL < 0 || DRL > 100 || tongTC < 0 || tbhk < 0 || tbhk > 4.0) {
+        return "Input khong hop le";
+    } else if (DRL < 70 || diemF == 1 || tongTC < 14 || tbhk < 3.2 || kyLuat == 1) {
+        return "Khong du dieu kien xet hoc bong";
+    } else {
+        return "Du dieu kien xet hoc bong";
+    }
+}
+
+
+
+int main() {
+    freopen("testcase.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    cout << "bat dau" << endl;
+    int x;
+    cin >> x;
+    cout << x << endl;
+    while (x--) {
+        cout << x << " ";
+        int DRL, tongTC;
+        bool diemF, kyLuat;
+        float tbhk;
+        cin >> DRL >> diemF >> tongTC >> tbhk >> kyLuat;
+        cout << xetHB(DRL,diemF,tongTC,tbhk,kyLuat) << endl;
+    }
+    return 0;
+}
